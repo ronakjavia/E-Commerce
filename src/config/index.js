626e75fa722140e45fd7a185b4c95ac45b0b364c
Ronak Javia <118874605+ronakjavia@users.noreply.github.com>
2023-04-17 14:00:09 +0000

    forgot Pswd functionalilty

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,10 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const config = {
-    PORT : process.env.PORT || 5000
-    MONGO_URL : process.env.MONGO_URL || 'mongodb://localhost:27017/e_commerce'
+    PORT : process.env.PORT || 5000,
+    MONGO_URL : process.env.MONGO_URL || 'mongodb://localhost:27017/e_commerce',
+    JWT_SECRET : process.env.JWT_SECRET || "yoursecret",
+    JWT_EXPIRY : process.env.JWT_EXPIRY || "10d",
 }
 
 exports.default = config;
--- src/models/userschema..js
+++ src/models/userschema..js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utils/authRoles.js"
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import config from "../config/index.js";
+import crypto from "crypto"
 
 const userSchema = mongoose.Schema({
     name: {
@@ -8,38 +11,60 @@ const userSchema = mongoose.Schema({
         required: [true, "Name required"],
         maxLength: [50, "Maximum Length 50 character"]
     },
-    email:{
+    email: {
         type: String,
         required: [true, "Email required"],
     },
-    password:{
+    password: {
         type: String,
         required: [true, "Pswd required"],
         minLength: [8, "Atleast 8 character"],
         select: false //It will not bring pswd along with info by default
     },
-    role:{
+    role: {
         type: String,
         enums: Object.values(AuthRoles),
-        default: AuthRoles.USER 
+        default: AuthRoles.USER
     },
     forgotPasswordToken: String,
     forgotPasswordExpiry: Date
-}, {timestamps:true})
+}, { timestamps: true })
 
 // Encrypt pswd before save
 
-userSchema.pre("save", async function(next){
-    if(!this.isModified("password")) return next()
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
     this.password = await bcrypt.hash(this.password, 10)
     next()
 }) // Can't use arrow function in some hooks in mongoose like pre
 
 userSchema.methods = {
     // compare pswd
-    comparePassword: async function(enteredPassword){
+    comparePassword: async function (enteredPassword) {
         return await bcrypt.compare(enteredPassword, this.password)
-    }
-}
+    },
+
+    // generating JsonWebToken
+    getJWT: function () {
+        jwt.sign({ _id: this._idd }, config.JWT_SECRET, {
+            expiresIN: config.JWT_EXPIRY
+        })
+    },
+
+    // forgot pswd
+    generateForgotPasswordToken: function () {
+        const forgotToken = crypto.randomBytes(20).toString('hex')
+
+        //ecrypt token
+        this.forgotPasswordToken = crypto
+        .createHash("sha256")
+        .update(forgotToken)
+        .digest("hex")
+
+        // time for token to expire
+        this.forgotPasswordExpiry = Date.now() + 20 * 60 *1000
+
+        return forgotToken
+    }}
 
 exports.default = mongoose.model("User", userSchema)
