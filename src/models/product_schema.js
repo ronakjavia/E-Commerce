c73263eb74f160b3a2b9ae13d63da66dcb011044
Ronak Javia <118874605+ronakjavia@users.noreply.github.com>
2023-04-17 14:34:57 +0000

    updated

---
+++ src/models/product_schema.js
@@ -0,0 +1,40 @@
+import mongoose, { mongo } from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide product name"],
+        trim: true,
+        maxLength: [120, "Maximum 120 characters"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Please provide product price"],
+        maxLength: [5, "Maximum 5 characters"]
+    },
+    description: {
+        type: String
+    },
+    photos: [
+        {
+            secure_url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    stock: {
+        type: Number,
+        default: 0
+    },
+    sold: {
+        type: Number,
+        default: 0
+    },
+    collectionId: {
+        ref: "Collection"
+    }
+
+}, { timestamps: true })
+
+exports.default = mongoose.model("product", productSchema)
